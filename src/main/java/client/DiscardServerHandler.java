a94f8d395a8427d724e28434f35d89e1841af956
hI <jktmobile6637@gmail.com>
2021-04-28 19:37:39 +0900

    ADD DiscardServerHandler

---
+++ src/main/java/client/DiscardServerHandler.java
@@ -0,0 +1,22 @@
+package client;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/*
+    서버 핸들러
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
